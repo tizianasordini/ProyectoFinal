3504df7259b3edfea3ea81a97ef732569092c1a0
tizianasordini <tizianasordini@hotmail.com>
2022-11-04 00:08:52 -0300

    menu de navegación stack para la pantalla de register

---
+++ final/src/navigation/MainNavigation.js
@@ -0,0 +1,24 @@
+import {NavigationContainer} from "@react-navigation/native"
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+
+import Register from "../screens/Register/Register"
+
+const Stack = createNativeStackNavigator()
+
+function MainNavigation() {
+    return (
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen
+            name='Register'
+            component={Register}
+            options={{
+                headerShown: false
+            }}
+            />
+        </Stack.Navigator>
+    </NavigationContainer>
+    )
+}
+
+export default MainNavigation;
