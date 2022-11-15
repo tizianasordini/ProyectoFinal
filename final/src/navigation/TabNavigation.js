c530ec27667b1ff1813451ea4bf742dc6a94048e
tizianasordini <tizianasordini@hotmail.com>
2022-11-14 23:11:34 -0300

    iconos y estilos de la navegacion

--- final/src/navigation/TabNavigation.js
+++ final/src/navigation/TabNavigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {FontAwesome} from '@expo/vector-icons'
 import Home from "../screens/Home/Home";
 import Posteo from "../screens/Posteo/Posteo";
 import Perfil from "../screens/Perfil/Perfil";
@@ -9,18 +10,30 @@ const Tab = createBottomTabNavigator()
 
 function TabNavigation() {
     return(
-        <Tab.Navigator>
+        <Tab.Navigator screenOptions={{tabBarShowLabel: false}}>
             <Tab.Screen
             name = "Home"
             component={Home}
+            options={{
+                tabBarIcon: () => <FontAwesome name='home' size={26} color="black" />,
+                headerShown:false
+            }}
             />
-              <Tab.Screen
+            <Tab.Screen
             name = "Posteo"
             component={Posteo}
+            options={{
+                tabBarIcon: () => <FontAwesome name='camera-retro' size={26} color="black" />,
+                headerShown:false
+            }}
             />
             <Tab.Screen
             name="Perfil"
             component={Perfil}
+            options={{
+                tabBarIcon: () => <FontAwesome name='user' size={26} color="black" />,
+                headerShown:false
+            }}
             />
 
         </Tab.Navigator>
